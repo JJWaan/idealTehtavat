beff42fe5b706b582e6739c93b3beacc27e8f9b9
JJWaan <j.waananen@protonmail.com>
2022-10-24 15:34:43 +0300

    chuck jokes app, with axios

--- huutoapp/src/App.js
+++ huutoapp/src/App.js
@@ -1,33 +1,105 @@
 import './App.css';
-import { useState, useReducer, useEffect } from "react"
-import axios from 'axios' // npm install axios , jos ei ole jo ladattu
+import { useReducer, useEffect } from "react"
+import axios from 'axios' // (npm install axios)
 
 function reducer(state, action) {
-  switch (action.type) {
-    case 'KATEGORIAT_NOUDETTU':
-      return {...state, kategoriat:action.payload}
-    case 'YYYY':
-    default:
-      throw new Error("Action.type kentän arvoa ei tunnistettu");
-  }
-}
-function AppHuuto() {
-  const [appData, dispatch] = useReducer(reducer, { kategoriat:[]});
 
+  switch (action.type) {
+// K = Kategoria
+// V = Vitsi
+    case 'K_NOUTO_ALOITETTU':
+      console.log("state: kategorioiden nouto aloitettu")
+      return { ...state, noutoAloitettu: true}
+
+    case 'K_NOUDETTU':
+      console.log("state: kategoriat noudettu jsonista")
+      return { ...state,
+        kategoriat: action.payload,
+        noutoAloitettu: false,
+        noutoValmis: true }
+
+    case 'V_NOUTO_ALOITETTU':
+      console.log("state: vitsin nouto aloitettu")
+      return { ...state,
+        noutoAloitettu: true,
+        noutoValmis: false }
+
+    case 'V_NOUDETTU':
+      console.log("state: vitsi noudettu")
+      return { ...state,
+        vitsi: action.payload,
+        noutoAloitettu: false,
+        noutoValmis: true }
+
+    case 'NOUTO_EPÄONNISTUI':
+      console.log("datan nouto epäonnistui")
+      return { ...state,
+        noutoEpäonnistui: true,
+        noutoAloitettu:false,
+        noutoValmis: false }
+
+    default:
+      throw new Error("Reducer action.type err");
+  }
+};
+
+// m a i n:
+function ChuckJokes() {
+  const [appData, dispatch] = useReducer(reducer, { vitsi: [], kategoriat: [], noutoAloitettu: false, noutoEpäonnistui: false, noutoValmis: false });
+// K = Kategoria
+// V = Vitsi
   useEffect(() => {
     async function haeDataa() {
-      let result = await axios('https://api.huuto.net/1.1/categories');
-      dispatch({type:'KATEGORIAT_NOUDETTU',payload:result.data.categories})
-      console.log(result.data.categories)
-    }
+      try {
+        dispatch({ type: 'K_NOUTO_ALOITETTU' })
+        // haetaan api:sta kategoriat categories-muuttujalle
+          let categories = await axios('https://api.chucknorris.io/jokes/categories');
+        // saadut kategoriat lähetetään payloadina reducerille
+        dispatch({ type: 'K_NOUDETTU', payload: categories.data })
+          // console.log("full json noudettu:", categories)
+          // console.log("noudettu categories:", categories.data) // (array)
+
+        dispatch({ type: 'V_NOUTO_ALOITETTU' })
+        // haetaan api:sta random vitsi joke-muuttujalle
+          let joke = await axios('https://api.chucknorris.io/jokes/random');
+        // vitsi itessään lähetään payloadina reducerille
+        dispatch({ type: 'V_NOUDETTU', payload: joke.data.value })
+          // console.log("noudettu vitsi json:", joke)
+          // console.log("vitsi.data.value:", joke.data.value)
+      }
+      catch (error) {
+        console.log("Los Proplemos:", error)
+        dispatch({ type: 'NOUTO_EPÄONNISTUI' })
+      }
+    };
+
     haeDataa();
+
   }, []);
 
   return (
-    <div>{appData.kategoriat.map(item=><div>{item.title}</div>)}
-      
-    </div>
-  );
-}
+    <>
+      <div className='outer-wrapper'>
 
-export default AppHuuto;
+        <div className='chuck-categories'>
+          <h3>Chuck Categories:</h3>
+          {appData.kategoriat.map(item => <p>{item}</p>)}
+        </div>
+
+        <div className='chuck-joke'>
+          <h3>Chuck Joke:</h3>
+          {appData.vitsi}
+        </div>
+
+      </div>
+
+      <div className='ui-info-box'>
+        {appData.noutoAloitettu && "UI notification: Nouto aloitettu" }
+        {appData.noutoEpäonnistui && "UI notification: Nouto epäonnistui" }
+        {appData.noutoValmis && "UI notification: Nouto on valmis" }
+      </div>
+    </>
+  );
+};
+
+export default ChuckJokes;
