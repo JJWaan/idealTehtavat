31d63150f74626062b4f17561c2799a8e5896b8f
JJWaan <j.waananen@protonmail.com>
2022-10-26 00:09:05 +0300

    reducer, useEffect, axios

--- huutoapp/src/App.js
+++ huutoapp/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import { useReducer, useEffect } from "react"
-import axios from 'axios' // (npm install axios)
+import axios from 'axios' // (npm i axios)
 
+// reducer, cases
 function reducer(state, action) {
-
   switch (action.type) {
 // K = Kategoria
 // V = Vitsi
+
+    // K:
     case 'K_NOUTO_ALOITETTU':
       console.log("state: kategorioiden nouto aloitettu")
-      return { ...state, noutoAloitettu: true}
+      return { ...state,
+        noutoAloitettu: true,
+        noutoValmis: false }
 
     case 'K_NOUDETTU':
       console.log("state: kategoriat noudettu jsonista")
@@ -18,6 +22,7 @@ function reducer(state, action) {
         noutoAloitettu: false,
         noutoValmis: true }
 
+    // V:
     case 'V_NOUTO_ALOITETTU':
       console.log("state: vitsin nouto aloitettu")
       return { ...state,
@@ -25,14 +30,21 @@ function reducer(state, action) {
         noutoValmis: false }
 
     case 'V_NOUDETTU':
-      console.log("state: vitsi noudettu")
+      console.log("state: vitsi noudettu jsonista")
       return { ...state,
-        vitsi: action.payload,
+        // vitsi: action.payload,
+        vitsiObjekti: action.payload,
         noutoAloitettu: false,
         noutoValmis: true }
 
+    case 'AUTOFETCH':
+      console.log("state: autofetching")
+      return { ...state,
+        // noutoAloitettu: true,
+        auto5: true }
+
     case 'NOUTO_EPÄONNISTUI':
-      console.log("datan nouto epäonnistui")
+      console.log("datan nouto epäonnistui", action.type)
       return { ...state,
         noutoEpäonnistui: true,
         noutoAloitettu:false,
@@ -45,9 +57,14 @@ function reducer(state, action) {
 
 // m a i n:
 function ChuckJokes() {
-  const [appData, dispatch] = useReducer(reducer, { vitsi: [], kategoriat: [], noutoAloitettu: false, noutoEpäonnistui: false, noutoValmis: false });
-// K = Kategoria
-// V = Vitsi
+  const [appData, dispatch] = useReducer(reducer, { 
+    vitsi: [], vitsiObjekti: {}, kategoriat: [],
+    noutoAloitettu: false,
+    noutoEpäonnistui: false,
+    noutoValmis: false,
+    auto5: false });
+
+  // main useEffect
   useEffect(() => {
     async function haeDataa() {
       try {
@@ -56,47 +73,126 @@ function ChuckJokes() {
           let categories = await axios('https://api.chucknorris.io/jokes/categories');
         // saadut kategoriat lähetetään payloadina reducerille
         dispatch({ type: 'K_NOUDETTU', payload: categories.data })
-          // console.log("full json noudettu:", categories)
-          // console.log("noudettu categories:", categories.data) // (array)
+        // console.log("noudettu categories:", categories.data) // (array)
+        // console.log("full json api:sta, /kategoriat noudettu:", categories)
 
         dispatch({ type: 'V_NOUTO_ALOITETTU' })
-        // haetaan api:sta random vitsi joke-muuttujalle
+        // haetaan api:sta random vitsi (objekti) joke-muuttujalle
           let joke = await axios('https://api.chucknorris.io/jokes/random');
-        // vitsi itessään lähetään payloadina reducerille
-        dispatch({ type: 'V_NOUDETTU', payload: joke.data.value })
+        // vitsi itessään lähetään payloadina reducerille (payload: joke.data.value)
+        // upd: napataan koko joke objektiksi
+        dispatch({ type: 'V_NOUDETTU', payload: joke })
           // console.log("noudettu vitsi json:", joke)
-          // console.log("vitsi.data.value:", joke.data.value)
+          // console.log("joke.data.value:", joke.data.value)
       }
       catch (error) {
         console.log("Los Proplemos:", error)
         dispatch({ type: 'NOUTO_EPÄONNISTUI' })
       }
     };
-
     haeDataa();
-
   }, []);
+  // end main useEffect
+
+  // auto5sec joke fetch
+  useEffect(() => {
+    appData.auto5 == true ? timeri : null;
+  }, []);
+
+  const timeri = () => {
+    setInterval(getRandomJK, 5000);
+  };
+
+  // clearInterval(timeri);
+
+  // onClick funktiot:
+  async function getRandomJK() {
+    try {
+      dispatch({ type: 'V_NOUTO_ALOITETTU' })
+      let joke = await axios('https://api.chucknorris.io/jokes/random');
+      dispatch({ type: 'V_NOUDETTU', payload: joke })
+    }
+    catch (error) {
+      console.log("Los Proplemos getRandomJK funktiosta:", error)
+      dispatch({ type: 'NOUTO_EPÄONNISTUI' })
+    }
+  };
+
+  // console.log("staten vitsiObjekti:", appData.vitsiObjekti)
+  // console.log("state (appData):", appData)
 
   return (
     <>
       <div className='outer-wrapper'>
 
-        <div className='chuck-categories'>
-          <h3>Chuck Categories:</h3>
-          {appData.kategoriat.map(item => <p>{item}</p>)}
+        <div className='chuck-wrapper'>
+
+          <div className='chuck-button'
+            onClick={() => {
+              getRandomJK();
+              console.log("random joke div click")
+            }}
+            >
+            get a random joke
+          </div>
+
+          <div className='chuck-button'
+            onClick={() => {
+              dispatch({ type: 'AUTOFETCH', payload: timeri })
+              console.log("new joke 5s div click")
+            }}
+            >
+            new joke every 5 secs
+          </div>
+
+          <div className='chuck-button'
+            onClick={() => {
+              console.log("localSto div click")
+            }}
+            >
+            jokes from localStorage
+          </div>
+
+          <div className='chuck-button'
+            onClick={() => {
+              console.log("challenge div click")
+            }}
+            >
+            time challenge
+          </div>
+
         </div>
 
-        <div className='chuck-joke'>
-          <h3>Chuck Joke:</h3>
-          {appData.vitsi}
+        <div className='chuck-wrapper'>
+
+          <div className='chuck-categories'>
+            <h3>Chuck Categories:</h3>
+              {appData.kategoriat.map(item => <p>{item}</p>)}
+          </div>
+
+          {/* <div className='chuck-joke'>
+            <h3>Random Chuck Joke:</h3>
+              <p>{appData.vitsi}</p>
+          </div> */}
+
+          <div className='chuck-joke'>
+            {appData.noutoValmis == true ?
+            <>
+              <h3>Random Chuck Joke:</h3>
+                <p>{appData.vitsiObjekti.data.value}</p>
+              <h4>Jokes ID:</h4>
+                <p>{appData.vitsiObjekti.data.id}</p>
+            </> : <p>Venataan fetchiä ('loading screen' tähän)</p>}
+          </div>
+
+          <div className='ui-info-box'>
+            {appData.noutoAloitettu && "UI notification: Nouto aloitettu" }
+            {appData.noutoEpäonnistui && "UI notification: Nouto epäonnistui" }
+            {appData.noutoValmis && "UI notification: Nouto on valmis" }
+          </div>
+
         </div>
 
-      </div>
-
-      <div className='ui-info-box'>
-        {appData.noutoAloitettu && "UI notification: Nouto aloitettu" }
-        {appData.noutoEpäonnistui && "UI notification: Nouto epäonnistui" }
-        {appData.noutoValmis && "UI notification: Nouto on valmis" }
       </div>
     </>
   );
