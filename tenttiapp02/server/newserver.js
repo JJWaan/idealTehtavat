05c0b3b23a8dc215df4a9ca3d32b4a272ef08269
JJWaan <j.waananen@protonmail.com>
2022-11-09 12:06:06 +0200

    http req methods with no hardcoded data

--- tenttiapp02/server/newserver.js
+++ tenttiapp02/server/newserver.js
@@ -8,54 +8,22 @@ app.use(bodyParser.urlencoded({ extended: true, }))
 const cors = require('cors');
 app.use(cors());
 
-const port = 8080;
+const port = process.env.PORT || 8080;
+// environment variable. commandline with
+// export PORT=5000 (set PORT=5000 for windows)
+// now PORT is env.variable. proper way to assing
+// ports to node apps. you should attempt to read value
+// of a env.value (called PORT)
+// otherwise use arbitrary number (8080) for a dev machine
 
 const funktiot = require('./newserverqueries');
 
 app.listen(port, () => { console.log(`listening on port ${port}`); });
+
 funktiot.poolStats();
 
-// rajapinta postmanille
-app.get('/', (request, response) => {
-  response.send('jotain');
-  console.log("jotain tapahtui")
-});
-
-// app.get('/', funktiot.getData);
-// app.post('/', funktiot.addData);
-// app.put('/', funktiot.updateData);
-// app.delete('/', funktiot.delData);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// POST to db
-// app.post('/', async (req, res) => {
-//     await pool.query("INSERT INTO oma_db (nimi) VALUES ('stuffia')");
-//     console.log("data written to db");
-//     res.send('data written to db');
-//     pool.end();
-// });
-
-  // Lisää backendiin, nimi-kolumniin (id tehdään pgAdmin:ssa)
-//   const lisaaValue = async () => {
-//     try {
-//       let result = await pool.query("INSERT INTO tentti_backend (nimi) VALUES ('jokisen asia')")
-//       console.log("insert (object) into backend:", result)
-//       console.log("insert command:", result.command)
-//       console.log("insert rowCount:", result.rowCount)
-//     } catch (error) { console.log("insert into Err:", error) }
-//     pool.end();
-//   }
-  // lisaaValue();
+app.get('/', funktiot.getData);
+app.get('/:id/', funktiot.getDataFromID);
+app.post('/', funktiot.addData);
+app.put('/:id', funktiot.updateData);
+app.delete('/:id', funktiot.deleteData);
--- tenttiapp02/server/newserverqueries.js
+++ tenttiapp02/server/newserverqueries.js
@@ -20,34 +20,66 @@ const poolStats = async () => {
 };
 
 // get, sql select from db
-const getData = (request, response) => {
-    console.log("ollaan getData funktiossa");
+const getData = async (request, response) => {
+    const sqlCommand = "SELECT * FROM oma_taulu"
     try {
-        pool.query("SELECT * FROM oma_taulu", (result) => {
-            response.status(200).json(result.rows);
-            console.log("resultti:", result.rows);
-        });
-    } catch { console.log("hmm", error); }
+        let stuff = await pool.query(sqlCommand)
+        response.status(200).send(stuff);
+        console.log('Query complete');
+    } catch (error) {
+        response.status(404).send('Not found')
+        console.log('Query error:', error);
+    }
     pool.end(() => { console.log('pool ended') })
 };
-
-// update data
-const updateData = async (request, response) => {
+//get based on id param
+const getDataFromID = async (request, response) => {
+    const { id } = request.params
+    const sqlCommand = "SELECT * FROM oma_taulu WHERE id=($1)"
     try {
-        pool.query("UPDATE oma_taulu SET boolean = '0' WHERE id=4", () => {
-        response.status(201).send();
-        });
-    } catch { console.log("hmm", error); }
+        let stuff = await pool.query(sqlCommand, [id])
+        response.status(200).send(stuff);
+        console.log('Query complete');
+    } catch (error) { response.status(404).send('Not found') }
     pool.end(() => { console.log('pool ended') })
 };
 
 // add data
 const addData = async (request, response) => {
+    const { teksti } = request.body
+    const sqlCommand = "INSERT INTO oma_taulu (teksti) VALUES ($1)"
+    const values = [teksti]
     try {
-        pool.query("INSERT INTO oma_taulu (teksti) VALUES 'uusi asia' WHERE id=4", () => {
-        response.status(201).send();
-        });
-    } catch { console.log("hmm", error); }
+        await pool.query(sqlCommand, values)
+        response.status(201).send('okay');
+        console.log('Data written');
+    } catch (error) { response.status(404).send('Not found') }
+    pool.end(() => { console.log('pool ended') })
+};
+
+// update data
+const updateData = async (request, response) => {
+    const { teksti } = request.body
+    const { id } = request.params
+    const sqlCommand = "UPDATE oma_taulu SET teksti=($1) WHERE id=($2)"
+    const values = [teksti, id]
+    try {
+        await pool.query(sqlCommand, values)
+        response.status(201).send('jee');
+        console.log('Data updated');
+    } catch (error) { response.status(404).send('Not found') }
+    pool.end(() => { console.log('pool ended') })
+};
+
+// delete data
+const deleteData = async (request, response) => {
+    const { id } = request.params
+    const sqlCommand = "DELETE FROM oma_taulu WHERE id=($1)"
+    try {
+        await pool.query(sqlCommand, [id])
+        response.status(201).send('Deleted by id #');
+        console.log('Data deleted');
+    } catch (error) { response.status(404).send('Not found') }
     pool.end(() => { console.log('pool ended') })
 };
 
@@ -55,6 +87,8 @@ const addData = async (request, response) => {
 module.exports = {
     poolStats,
     getData,
+    getDataFromID,
     updateData,
     addData,
+    deleteData,
 };
