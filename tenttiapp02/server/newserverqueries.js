2c6f0b4520dadccd8a693f23d342698d932be935
JJWaan <j.waananen@protonmail.com>
2022-11-11 13:46:07 +0200

    updated sql/functions before database switch

--- tenttiapp02/server/newserverqueries.js
+++ tenttiapp02/server/newserverqueries.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+const { Pool, Client } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
@@ -12,7 +12,20 @@ const pool = new Pool({
     // allowExitOnIdle: true,
 });
 
-// statistics, properties
+// const client = new Client({
+//     host: 'localhost',
+//     database: 'oma_db',
+//     user: 'postgres',
+//     password: 'admin',
+//     port: 5432,
+// });
+
+// client.connect(err => {
+//     if (err) { console.error('client connection error', err.stack)
+//     } else { console.log('client connected') }
+// });
+
+// pool statistics, properties
 const poolStats = async () => {
     console.log('# of total clients existing in pool:', pool.totalCount)
     console.log('# of total clients idle in pool:', pool.idleCount)
@@ -25,21 +38,21 @@ const getData = async (request, response) => {
         const { id } = request.params
         const sqlCommand = "SELECT * FROM oma_taulu WHERE id=($1)"
         try {
-            let result = await pool.query(sqlCommand, [id])
+            let result = await client.query(sqlCommand, [id])
             response.status(200).send(result);
             console.log('Query complete');
-            return;
         } catch (error) {
             response.status(404).send('Not found')
             console.log('Query failed:', error);
         }
         pool.end(() => { console.log('pool ended') })
+        return;
     };
 
     const sqlCommand = "SELECT * FROM oma_taulu"
     try {
-        let stuff = await pool.query(sqlCommand)
-        response.status(200).send(stuff);
+        let result = await client.query(sqlCommand)
+        response.status(200).send(result);
         console.log('Query complete');
     } catch (error) {
         response.status(404).send('Not found')
@@ -55,7 +68,7 @@ const addData = async (request, response) => {
     const values = [teksti]
     try {
         await pool.query(sqlCommand, values)
-        response.status(201).send('okay');
+        response.status(201).send('Data inserted succesfully');
         console.log('Data written');
     } catch (error) { response.status(404).send('Not found') }
     pool.end(() => { console.log('pool ended') })
@@ -63,15 +76,15 @@ const addData = async (request, response) => {
 
 // update data
 const updateData = async (request, response) => {
-    if (!request.body || request.body.length < 1 || !request.params) {
-        response.status(400).send('Bad request')
-        return;
-    }
-    const { teksti } = request.body
-    const { id } = request.params
-    const sqlCommand = "UPDATE oma_taulu SET teksti=($1) WHERE id=($2)"
-    const values = [teksti, id]
     try {
+        if (!request.body.teksti || request.body.teksti.length < 1) {
+            response.status(400).send('Bad request, "teksti" is needed')
+            return;
+        }
+        const { teksti } = request.body
+        const { id } = request.params
+        const sqlCommand = "UPDATE oma_taulu SET teksti=($1) WHERE id=($2)"
+        const values = [teksti, id]
         await pool.query(sqlCommand, values)
         response.status(201).send('Data updated succesfully');
         console.log('Data updated');
